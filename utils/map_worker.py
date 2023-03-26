ed25d77d687b6ab0ccd8119f1853c187ed33c67a
L33t <maxim.y.mischenko@gmail.com>
2023-03-26 18:27:20 +0500

    done map worker

--- utils/map_worker.py
+++ utils/map_worker.py
@@ -2,16 +2,23 @@ import osmnx as ox
 import networkx as nx
 import folium
 
+from osmnx import settings
+
+from geopy.geocoders import Nominatim
+
 
 class MapWorker:
     def __init__(self, start_location, end_location, region):
-        ox.config(log_console=True, use_cache=True)
-        self.start_location = start_location
-        self.end_location = end_location
+        settings.log_console = True
+        settings.use_cache = True
+
+        self.locator = Nominatim(user_agent='lol')
+        self.start_location = self.calculate_location(start_location)
+        self.end_location = self.calculate_location(end_location)
         self.region = region
         self.mode = 'walk'  # 'drive', 'bike', 'walk'
         self.optimizer = 'time'  # 'length', 'time'
-        self.graph = None
+        self.graph = self.calculate_graph()
 
     def set_start_location(self, new_location):
         self.start_location = new_location
@@ -29,26 +36,18 @@ class MapWorker:
         self.region = new_region
 
     def calculate_graph(self):
-        self.graph = ox.graph_from_place(self.region, network_type=self.mode)
+        return ox.graph_from_place(self.region, network_type=self.mode)
 
     def find_nearest_node(self):
-        if self.graph is None:
-            self.calculate_graph()
         return ox.nearest_nodes(self.graph, *self.start_location[::-1])
 
     def find_destination_node(self):
-        if self.graph is None:
-            self.calculate_graph()
         return ox.nearest_nodes(self.graph, *self.end_location[::-1])
 
     def calculate_shortest_route(self):
-        if self.graph is None:
-            self.calculate_graph()
         original_node = self.find_nearest_node()
         destination_node = self.find_destination_node()
-        print(original_node, destination_node)
-
-        # method: dijkstra, bellman-ford
+        # approach: dijkstra, bellman-ford
         return nx.shortest_path(self.graph, original_node, destination_node, weight=self.optimizer)
 
     def get_plot(self):
@@ -62,13 +61,31 @@ class MapWorker:
         folium.TileLayer('cartodbpositron').add_to(shortest_route_map)
         folium.TileLayer('cartodbdark_matter').add_to(shortest_route_map)
         folium.LayerControl().add_to(shortest_route_map)
+
+        start_marker = folium.Marker(
+            location=self.start_location,
+            popup=self.start_location,
+            icon=folium.Icon(color='green')
+        )
+        end_marker = folium.Marker(
+            location=self.end_location,
+            popup=self.end_location,
+            icon=folium.Icon(color='red')
+        )
+        start_marker.add_to(shortest_route_map)
+        end_marker.add_to(shortest_route_map)
+
         return shortest_route_map
+
+    def calculate_location(self, location):
+        location_coordinates = self.locator.geocode(location)
+        return location_coordinates.latitude, location_coordinates.longitude
 
 
 if __name__ == '__main__':
-    start_location = (56.848114, 60.600726)
-    end_location = (57.001182, 60.520375)
+    start_location = 'площадь 1905 года'
     region = 'Екатеринбург'
+    end_location = 'Екатеринбургский государственный академический театр оперы и балета'
     mw = MapWorker(start_location, end_location, region)
     plot = mw.get_plot()
     plot.save('map.html')
